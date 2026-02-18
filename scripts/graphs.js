1e42577ac4402c82e60442429be2fbde09077af0
Merissa Bridgeman <merissa.bridgeman98@gmail.com>
2026-02-18 01:15:55 -0800

    changed max value back to 20

--- scripts/graphs.js
+++ scripts/graphs.js
@@ -147,7 +147,7 @@ function renderLine(canvas, labels, datasets, title) {
       interaction: { mode: "nearest", intersect: false },
       plugins: { title: { display: true, text: title } },
       scales: { y: { 
-        max: 15,
+        max: 20,
         ticks: { callback: (v) => Number(v).toFixed(2) } 
       } 
     }
